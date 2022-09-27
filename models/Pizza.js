e6796b9af013a507e9f34591edba316c58898d9e
Aaron Key <aaronmikelkey@gmail.com>
2022-09-27 19:00:08 -0400

    Add mongoose, pizza model

---
+++ models/Pizza.js
@@ -0,0 +1,19 @@
+import { Schema, model } from "mongoose";
+
+export default PizzaSchema = new Schema({
+  pizzaName: {
+    type: String,
+  },
+  createdBy: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  size: {
+    type: String,
+    default: "Large",
+  },
+  toppings: [],
+});
